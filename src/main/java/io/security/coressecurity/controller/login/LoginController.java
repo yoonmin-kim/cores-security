ef3824b335ce4ba32784012e2c5264098392cc45
YoonMin <rladbsals23@gmail.com>
2021-06-13 22:06:07 +0900

    feat : 로그아웃 및 화면 보안처리
    [로그아웃]
    · form태그 post방식
     - LogoutFilter에서 동작하게됨
    
    · a태그 get방식
     - 별도의 컨트롤러를 작성해서 SecurityContextLogoutHandler활용
     - LogoutFilter에서도 SecurityContextLogoutHandler를 활용함

--- src/main/java/io/security/coressecurity/controller/login/LoginController.java
+++ src/main/java/io/security/coressecurity/controller/login/LoginController.java
@@ -1,7 +1,13 @@
 package io.security.coressecurity.controller.login;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 public class LoginController {
@@ -11,4 +17,13 @@ public class LoginController {
         return "user/login/login";
     }
 
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            new SecurityContextLogoutHandler().logout(request, response, authentication);
+        }
+        return "redirect:/login";
+    }
 }
