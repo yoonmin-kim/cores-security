94c245c94bbfd5ab3eb999df3342ec1bad0506d0
YoonMin <rladbsals23@gmail.com>
2021-06-18 22:09:46 +0900

    feat : 인증 핸들러 - AjaxAuthenticationSuccessHandler,                    AjaxAuthenticationFailureHandler

--- src/main/java/io/security/coressecurity/security/config/AjaxSecurityConfig.java
+++ src/main/java/io/security/coressecurity/security/config/AjaxSecurityConfig.java
@@ -1,6 +1,8 @@
 package io.security.coressecurity.security.config;
 
 import io.security.coressecurity.security.filter.AjaxLoginProcessingFilter;
+import io.security.coressecurity.security.handler.AjaxAuthenticationFailureHandler;
+import io.security.coressecurity.security.handler.AjaxAuthenticationSuccessHandler;
 import io.security.coressecurity.security.provider.AjaxAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
@@ -23,18 +27,28 @@ public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(auThenticationProvider());
-    }
-
-    private AuthenticationProvider auThenticationProvider() {
+    private AuthenticationProvider authenticationProvider() {
         return new AjaxAuthenticationProvider(userDetailsService, passwordEncoder());
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler() {
+        return new AjaxAuthenticationSuccessHandler();
+    }
+
+    @Bean
+    public AuthenticationFailureHandler authenticationFailureHandler() {
+        return new AjaxAuthenticationFailureHandler();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
@@ -51,6 +65,8 @@ public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
     public AbstractAuthenticationProcessingFilter authenticationProcessingFilter() throws Exception {
         AjaxLoginProcessingFilter ajaxLoginProcessingFilter = new AjaxLoginProcessingFilter();
         ajaxLoginProcessingFilter.setAuthenticationManager(authenticationManagerBean());
+        ajaxLoginProcessingFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler());
+        ajaxLoginProcessingFilter.setAuthenticationFailureHandler(authenticationFailureHandler());
         return ajaxLoginProcessingFilter;
     }
 }
